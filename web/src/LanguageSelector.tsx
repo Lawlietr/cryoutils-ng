e22be664d6417b72015adefc9b36bd758e652a7e
Mulder Lee <lawliet@noreply.localhost>
2026-08-11 23:29:38 +0800

    fix(web): import Locale type in LanguageSelector

--- web/src/LanguageSelector.tsx
+++ web/src/LanguageSelector.tsx
@@ -1,5 +1,5 @@
 import { useI18n } from './i18n'
-import { LOCALES } from './i18n/index'
+import { LOCALES, type Locale } from './i18n/index'
 
 export default function LanguageSelector() {
   const { locale, setLocale } = useI18n()
